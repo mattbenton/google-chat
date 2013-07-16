998e8a8c891bb71f484fb00be1a6e4e1b88c96b4
Matt Benton <matt@mattbenton.net>
2013-07-16 10:00:44 -0700

    Cleaned server code

--- server/server.js
+++ server/server.js
@@ -3,34 +3,12 @@
 var io = require('socket.io').listen(8080);
 
 io.configure(function() {
-  io.set('log level', 1);
+  io.set('log level', 2);
 });
-
-var users = {};
-
-// var clients = {};
 
 io.sockets.on('connection', function ( socket ) {
   socket.on('msg', function ( msg ) {
+    console.log('broadcast: ', msg);
     socket.broadcast.emit('msg', msg);
   });
-
-  // socket.on('login', function( user ) {
-  //   console.log('login: ', user);
-  //   var sockets = users[user] || [];
-  //   sockets.push(socket);
-  //   users[user] = sockets;
-
-  //   sendToUser(user);
-  // });
 });
-
-// function sendToUser ( user ) {
-//   var sockets = users[user];
-//   if ( sockets ) {
-//     console.log('sending to user `%s`', user);
-//     sockets.forEach(function ( socket ) {
-//       socket.send('hello from server!');
-//     });
-//   }
-// }
